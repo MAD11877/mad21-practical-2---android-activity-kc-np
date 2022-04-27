0edbc51c8e5b8fc2d104f420190139670cd4c057
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2022-04-27 17:45:11 +0800

    practical 2 answer - 2

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -0,0 +1,42 @@
+package sg.edu.np.mad.madpractical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    User user = new User("Tom", "description", 1, false);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initFollowBtnTxt(user);
+
+    }
+
+    public void onClick(View v){
+        Button followBtn = findViewById(R.id.buttonFollow);
+        if (followBtn.getText() == "Follow"){
+            followBtn.setText("Unfollow");
+        }
+        else {
+            followBtn.setText("Follow");
+        }
+        user.setFollowed(!user.isFollowed());
+    }
+
+    public void initFollowBtnTxt(User user) {
+        Button followBtn = findViewById(R.id.buttonFollow);
+        if (!user.isFollowed()){
+            followBtn.setText("Follow");
+        }
+        else {
+            followBtn.setText("Unfollow");
+        }
+    }
+}
